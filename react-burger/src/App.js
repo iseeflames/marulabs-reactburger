78878708363948b66344de5a65922af303c745c2
scarface <pphiri1012x@gmail.com>
2022-02-01 07:12:27 +0200

    Style top bread

--- react-burger/src/App.js
+++ react-burger/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import BottomBread from "./components/BottomBread/BottomBread";
 import Meat from "./components/Meat/Meat";
+import TopBread from "./components/TopBread/TopBread";
 import Vegetable from "./components/Vegetable/Vegetable";
 
 function App() {
   return (
     <div className="App">
+      <TopBread />
       <Meat name="Beef" />
       <Vegetable name="Tomato" />
       <BottomBread />
---
+++ react-burger/src/components/TopBread/TopBread.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./TopBread.css"
+
+export default function TopBread() {
+  return (
+    <div className="topbread-container">
+      
+    </div>
+  )
+}
